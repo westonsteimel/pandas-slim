8055384d1259038c64b6a4bb95d8cf672f1d6e60
Weston Steimel <weston.steimel@gmail.com>
2021-01-12 00:38:03 +0000

    use 3.7 as base image to get around strange bug with latest version of pandas not available
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM python:3.9-slim
+# For some reason using `pip download` with the `--python-version`
+# option does not find the most recent version of pandas for 
+# the lowest supported python version (currently 3.7) so we use that as base
+# image
+
+FROM python:3.7-slim
 
 RUN apt update \
     && apt install -y \
--- slimify.sh
+++ slimify.sh
@@ -19,7 +19,10 @@ mkdir -p dist
     
     $PIP_DOWNLOAD_CMD --python-version 3.9 --platform manylinux1_x86_64 pandas==${PANDAS_VERSION}
     $PIP_DOWNLOAD_CMD --python-version 3.8 --platform manylinux1_x86_64 pandas==${PANDAS_VERSION}
-    $PIP_DOWNLOAD_CMD --python-version 3.7 --platform manylinux1_x86_64 pandas==${PANDAS_VERSION}
+    
+    # We can't specify `--python-version` for the 3.7 version because there is some strange bug which prevents
+    # it from finding the latest version of pandas in this case
+    $PIP_DOWNLOAD_CMD --platform manylinux1_x86_64 pandas==${PANDAS_VERSION}
 
     for filename in ./*.whl
     do
